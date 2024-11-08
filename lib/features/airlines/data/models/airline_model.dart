3dacba5c45370d2938a9aef4a5ac52e6b566754d
Ahmed Mahrous <ahmedmahrous768@gmail.com>
2024-11-08 12:46:02 +0200

    creating airline model

---
+++ lib/features/airlines/data/models/airline_model.dart
@@ -0,0 +1,29 @@
+import 'package:equatable/equatable.dart';
+
+class AirlineModel extends Equatable {
+  final String? name;
+  final String? logo;
+  final String? phone;
+  final String? website;
+  final bool? isFav;
+
+  AirlineModel(
+      {required this.name,
+      required this.logo,
+      required this.phone,
+      required this.website,
+      required this.isFav});
+
+  factory AirlineModel.fromJson(Map<String, dynamic> json) {
+    return AirlineModel(
+      name: json['name'],
+      logo: json['logoURL'],
+      phone: json['phone'],
+      website: json['site'],
+      isFav: false,
+    );
+  }
+
+  @override
+  List<Object?> get props => [name, logo, phone, website, isFav];
+}
